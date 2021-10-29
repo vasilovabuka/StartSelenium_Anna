477c0eb5728bdb4e4de0c81fc3cae50b9243675e
vasilovabuka <92602944+vasilovabuka@users.noreply.github.com>
2021-10-29 23:04:38 +0300

    commit 2

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+    WebDriver wd;
+
+
+
+
+    @BeforeMethod
+        public void setUp() throws InterruptedException {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+
+
+  // wd=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/Users/roiz/Documents/GitHub/Untitled/StartSelenium_Anna/chromedriver");
+   //wd.get("https://contacts-app.tobbymarshall815.vercel.app/login");
+   wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/login");
+   //wd.navigate().forward();
+
+    }
+    @Test
+    public void testName(){
+
+    }
+
+    @Test
+     public void testName2(){
+
+    }
+    @AfterMethod
+    public void postCondition(){
+        //wd.close();
+        //wd.quit();
+
+    }
+}
